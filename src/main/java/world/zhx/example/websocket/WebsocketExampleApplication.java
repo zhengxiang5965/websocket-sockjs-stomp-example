9c27cc2c19f8c91a2e44b0c7300135eb7800285c
ZhengXiang <472010602@qq.com>
2018-07-05 15:12:47 +0800

    删除了部分空行

--- src/main/java/world/zhx/example/websocket/WebsocketExampleApplication.java
+++ src/main/java/world/zhx/example/websocket/WebsocketExampleApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebsocketExampleApplication {
-
-
     public static void main(String[] args) {
         SpringApplication.run(WebsocketExampleApplication.class, args);
     }
