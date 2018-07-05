efbf79b36fb531d15a0accd52a5f8b68a425e462
ZhengXiang <472010602@qq.com>
2018-07-05 14:59:49 +0800

    第一个测试版本

--- src/main/java/world/zhx/example/websocket/WebsocketExampleApplication.java
+++ src/main/java/world/zhx/example/websocket/WebsocketExampleApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebsocketExampleApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(WebsocketExampleApplication.class, args);
     }
